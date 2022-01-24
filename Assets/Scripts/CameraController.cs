7a1167f1895360b225d8507934ab973d44058b25
Igor Neres Trindade <igornerest@gmail.com>
2022-01-24 00:39:43 -0300

    Implement grid movement with mouse drag

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,29 +2,92 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float panSpeed = 10f;
-    [SerializeField] private float scrollSpeed = 40f;
+    [SerializeField] private float keyboardPanSpeed = 10f;
+    [SerializeField] private float mousePanSpeed = 4f;
+    [SerializeField] private float mouseScrollSpeed = 40f;
 
-    [SerializeField] private Vector2 panLimit = new Vector2(2,2);
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 7f;
+
+    [SerializeField] private float minY = -1f;
+    [SerializeField] private float maxY = 5f;
+
     [SerializeField] private float minHeight = 3f;
-    [SerializeField] private float maxHeight = 10f;
+    [SerializeField] private float maxHeight = 7f;
 
-    void Update()
+    private Vector3 startPos;
+
+    private bool isBeingHeld = false;
+
+    private void Update()
+    {
+        HandleMouseMovement();
+        HandleMouseScroll();
+        ClampWorldBoundaries();
+    }
+
+    private void ClampWorldBoundaries()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.z = Mathf.Clamp(pos.z, minY, maxY);
+        pos.y = Mathf.Clamp(pos.y, minHeight, maxHeight);
+
+        transform.position = pos;
+    }
+
+    private void HandleMouseMovement()
+    {
+        // OnMouseDown, onMouseUp and onMouseDrag were not used due to the
+        // dependency on collider and rigidbodies to work properly
+        if (Input.GetMouseButton(0))
+        {
+            if (!isBeingHeld)
+            {
+                startPos = transform.position + GetMousePosition() * mousePanSpeed;
+                isBeingHeld = true;
+            }
+            else
+            {
+                transform.position = startPos - GetMousePosition() * mousePanSpeed;
+            }
+        }
+        else if (isBeingHeld)
+        {
+            isBeingHeld = false;
+        }
+    }
+
+    private void HandleMouseScroll()
+    {
+        Vector3 pos = transform.position;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y += scroll * mouseScrollSpeed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+
+    private void HandleKeyboardMovement()
     {
         Vector3 pos = transform.position;
 
         float horizontalMove = Input.GetAxis("Horizontal");
+        pos.x += horizontalMove * keyboardPanSpeed * Time.deltaTime;
+
         float verticalMove = Input.GetAxis("Vertical");
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        pos.x += horizontalMove * panSpeed * Time.deltaTime;
-        pos.z += verticalMove * panSpeed * Time.deltaTime;
-        pos.y += scroll * scrollSpeed * Time.deltaTime;
-
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
-        pos.y = Mathf.Clamp(pos.y, minHeight, maxHeight);
+        pos.z += verticalMove * keyboardPanSpeed * Time.deltaTime;
 
         transform.position = pos;
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mousePos =  Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        mousePos = Camera.main.ScreenToViewportPoint(mousePos);
+
+        return new Vector3(mousePos.x, 0, mousePos.y);
     }
 }
